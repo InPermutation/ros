8da6c0b0f88952d478f72b204ef18c51d86ccd96
Jacob Krall <jacob@jkrall.net>
2015-10-27 19:54:02 -0400

    Entering Long Mode: `error` procedure

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -3,6 +3,14 @@ global start
 section .text
 bits 32
 start:
-    ; print `OK` to screen
-    mov dword [0xb8000], 0x2f4b2f4f
+    mov al, 0x58
+    jmp error
+
+; Prints `ERR: ` and the given error code to screen and hangs.
+; parameter: error code (in ASCII) in al
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f004f20
+    mov byte  [0xb800a], al
     hlt
