e370092ec644b07a4780a292e693f7e4f55fc262
Jacob Krall <jacob@jkrall.net>
2015-10-27 21:07:42 -0400

    Entering Long Mode: test_cpuid (ERR: 1)

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -7,6 +7,7 @@ start:
     mov esp, stack_top
 
     call test_multiboot
+    call test_cpuid
 
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
@@ -27,6 +28,24 @@ test_multiboot:
     ret
 .no_multiboot:
     mov al, "0"
+    jmp error
+
+test_cpuid:
+    pushfd               ; Store the FLAGS-register.
+    pop eax              ; Restore the A-register.
+    mov ecx, eax         ; Set the C-register to the A-register.
+    xor eax, 1 << 21     ; Flip the ID-bit, which is bit 21.
+    push eax             ; Store the A-register.
+    popfd                ; Restore the FLAGS-register.
+    pushfd               ; Store the FLAGS-register.
+    pop eax              ; Restore the A-register.
+    push ecx             ; Store the C-register.
+    popfd                ; Restore the FLAGS-register.
+    xor eax, ecx         ; Do a XOR-operation on the A-register and the C-register.
+    jz .no_cpuid         ; The zero flag is set, no CPUID.
+    ret                  ; CPUID is available for use.
+.no_cpuid:
+    mov al, "1"
     jmp error
 
 ; Reserve space for stack
