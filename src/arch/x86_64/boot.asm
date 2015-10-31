39a80c91c257fe98f1147a126c9a832015be3abf
Jacob Krall <jacob@jkrall.net>
2015-10-30 21:18:46 -0400

    setup page tables and enable paging

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -11,6 +11,9 @@ start:
     call test_cpuid
     call test_long_mode
 
+    call setup_page_tables
+    call enable_paging
+
     call print_vendorid
 
     hlt
@@ -101,8 +104,66 @@ test_long_mode:
     mov al, "2"
     jmp error
 
+setup_page_tables:
+    ; map first P4 entry to P3 table
+    mov eax, p3_table
+    or eax, 0b11 ; writable|present
+    mov [p4_table], eax
+
+    ; map first P3 entry to P2 table
+    mov eax, p2_table
+    or eax, 0b11 ; writable|present
+    mov [p3_table], eax
+
+    ; identity-map each P2 entry to a huge 2MiB page
+    mov ecx, 0 ; counter variable
+
+.map_p2_table:
+    ; map ecx'th P2 entry to a huge table that starts at address 2MiB*ecx
+    mov eax, 0x200000  ; 2MiB
+    imul eax, ecx      ; start address of ecx'th page
+    or eax, 0b10000011 ; huge|writable|present
+    mov [p2_table + ecx * 8], eax ; map ecx'th entry
+
+    inc ecx            ; increase counter
+    cmp ecx, 512       ; if counter==512, the whole P2 table is mapped
+    jne .map_p2_table  ; else map the next entry
+
+    ret
+
+enable_paging:
+    ; load P4 to cr3 register (cpu uses this to access the P4 table)
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE-flag in c4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model-specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ;enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    or eax, 1 << 16
+    mov cr0, eax
+
+    ret
+
 ; Reserve space for stack
 section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
 stack_bottom:
     resb 64
 stack_top:
