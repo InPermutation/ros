0a2e75fa54eb766f8032ef991367244ffcab846d
Jacob Krall <jacob@jkrall.net>
2015-10-31 01:09:35 -0400

    Support formatting macros

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,7 @@
 use core::ptr::Unique;
+use core::fmt;
+use core::fmt::Write;
+use core::result;
 
 #[repr(u8)]
 pub enum Color {
@@ -92,6 +95,16 @@ pub fn print_something() {
 
     writer.write_byte(b'H');
     writer.write_byte(b'i');
-    writer.write_str(", there!")
+    writer.write_str(", there!");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0);
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for byte in s.bytes() {
+            self.write_byte(byte)
+        }
+        Ok(())
     }
 }
